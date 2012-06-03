8104d6cdb03e61edf205ebd6bac78469ffb2d584
Gerardo Lopez-Fernandez <gerir@evernote.com>
2012-06-02 22:08:27 -0700

    pdlist and ldpdinfo

--- lib/elesai/lsi.rb
+++ lib/elesai/lsi.rb
@@ -1,5 +1,3 @@
-require 'ostruct'
-
 module Elesai
 
   class LSIArray
@@ -13,8 +11,8 @@ module Elesai
       @enclosures = []
       @spans = []
 
-      megacli = PDinfo_aAll.new
-      megacli.parse!(self,MEGACLI_PDINFO_AALL_OUT)
+      megacli = LDPDinfo_aAll.new
+      megacli.parse!(self,MEGACLI_LDPDINFO_AALL_OUT)
     end
 
     def add_adapter(a)
@@ -27,6 +25,7 @@ module Elesai
 
     def add_physicaldrive(pd)
       @physicaldrives[pd._id] = pd if @physicaldrives[pd._id].nil?
+      @physicaldrives[pd._id]
     end
 
     def to_s
@@ -43,17 +42,39 @@ module Elesai
       lsiarrayout
     end
 
+    ### Adapter
+
     class Adapter < Hash
 
-      def inspect
-        "#{self.class}:#{self.__id__}"
+      def initialize
+        self[:virtualdrives] = []
+        self[:physicaldrives] = {}
+        super
       end
 
       def _id
         "#{self[:id]}"
       end
 
+      def type
+        :adapter
+      end
+
+      def type_of?(type)
+        self.type == type
+      end
+
+      def inspect
+        "#{self.class}:#{self.__id__}"
+      end
+
+      def add_physicaldrive(pd)
+        self[:physicaldrives][pd._id] = pd unless self[:physicaldrives][pd._id].nil?
+      end
+
     end
+
+    ### Virtual Drive
 
     class VirtualDrive < Hash
 
@@ -65,18 +86,40 @@ module Elesai
           :offline              => 'Offline'
       }
 
-      class Size < Struct.new(:number, :unit); end
-      class RaidLevel < Struct.new(:primary, :secondary); end
+      class Size < Struct.new(:number, :unit)
+        def to_s ; "%8.2f%s" % [self.number,self.unit] end
+      end
+      class RaidLevel < Struct.new(:primary, :secondary)
+        def to_s ; "%s:%s" % [self.primary,self.secondary] end
+      end
+
+      def initialize
+        self[:physicaldrives] = []
+      end
+
+      def _id
+        self[:targetid]
+      end
+
+      def type
+        :virtualdrive
+      end
+
+      def type_of?(type)
+        self.type == type
+      end
 
       def inspect
         "#{self.class}:#{self.__id__}"
       end
 
+      def add_physicaldrive(pd)
 
+      end
 
-#      def to_s
-#        "[VD] %4s %18s %7.2f %s %d" % [ @id, @state, @size, self.raidlevel, @physicaldrives.size ]
-#      end
+      def to_s
+        "[VD] %4s %18s %s %s %d" % [ self._id, self[:state], self[:size], self[:raidlevel], self[:physicaldrives].size ]
+      end
 
     end
 
@@ -99,21 +142,61 @@ module Elesai
           :spun_up              => 'Spun up'
       }
 
+      class Size < Struct.new(:number, :unit)
+        def to_s ; "%8.2f%s" % [self.number,self.unit] end
+      end
+      class FirmwareState < Struct.new(:state, :spin)
+        def to_s
+          "#{self.state}:#{self.spin}"
+        end
+      end
+
+      def initialize
+        self[:_adapter] = nil
+        self[:_virtualdrives] = []
+      end
+
       def _id
         "e#{self[:enclosuredeviceid].to_s}s#{self[:slotnumber].to_s}".to_sym
       end
 
+      def type
+        :physicaldrive
+      end
+
+      def type_of?(type)
+        self.type == type
+      end
+
       def to_s
-        self.__id__
+        keys = [:deviceid, :firmwarestate, :coercedsize, :mediatype, :pdtype, :mediaerrorcount, :predictivefailurecount,:inquirydata]
+        #"[PD] %8s %4s %19s %8.2f%s %5s %5s %3d %3d   %s" % [ self.id, @deviceid, "#{@state}:#{@spin}", @_size.number, @_size.unit, @mediatype, @pdtype, @mediaerrors, @predictivefailure, @inquirydata  ]
+        "[PD] %8s %4s %19s %s %5s %5s %3d %3d  a%s  %s" % [ self._id, self[:deviceid], self[:firmwarestate], self[:coercedsize], self[:mediatype], self[:pdtype], self[:mediaerrorcount], self[:predictivefailurecount], self[:_adapter]._id, self[:inquirydata] ]
       end
 
       def inspect
         "#{self.class}:#{self.__id__}"
       end
 
-      class Size < Struct.new(:number, :unit); end
-      class FirmwareState < Struct.new(:state, :spin); end
+      def add_adapter(a)
+        self[:_adapter] = a
+      end
 
+      def get_adapter
+        self[:_adapter]
+      end
+
+      def add_virtualdrive(vd)
+         self[:_virtualdrives][vd._id] = vd if self[:_virtualdrives][vd._id].nil?
+      end
+
+      def get_virtualdrive(vd_id)
+        self[:_virtualdrives][vd_id]
+      end
+
+      def get_virtualdrives
+        self[:_virtualdrives]
+      end
     end
   end
 end
--- lib/elesai/megacli.rb
+++ lib/elesai/megacli.rb
@@ -18,40 +18,56 @@ module Elesai
     class Context
 
       def initialize(current_state,lsi)
-        current_state.meta[:context] = []
+        current_state.meta[:context] = { :stack => [], :adapter => nil, :virtualdrive => nil, :physicaldrive => nil }
         @context = current_state.meta[:context]
         @lsi = lsi
+      end
+
+      def open(component)
+        puts "         * Open #{component.inspect}"
+        @context[:stack].push(component)
+        @context[component.type] = component
+        puts "           + context: #{@context[:stack]}"
       end
 
       def flash!(new_state)
         new_state.meta[:context] = @context
         @context = nil
         @context = new_state.meta[:context]
-        puts "         + Flash context: #@context"
-      end
-
-      def open(component)
-        puts "         + Push #{component.inspect}"
-        @context.push(component)
-        puts "         + context: #@context"
+        puts "         + Flash context: #{@context[:stack]}"
       end
 
       def close
-        component = @context.pop
-        puts "         + Pop #{component.inspect}"
-        puts "         * Closing #{component.inspect}"
-        if component.kind_of? LSIArray::PhysicalDrive
-          @lsi.add_physicaldrive(component)
-        elsif component.kind_of? LSIArray::VirtualDrive
-          id = component._id
-        elsif component.kind_of? LSIArray::Adapter
+        component = @context[:stack].pop
+        @context[component.type] = nil
+        puts "         * Close #{component.inspect}"
+        if component.type_of? :physicaldrive
+          pd = @lsi.add_physicaldrive(component)
+          pd.add_adapter(adapter)
+          pd.add_virtualdrive(virtualdrive) unless virtualdrive.nil?
+          adapter.add_physicaldrive(pd)
+        elsif component.type_of? :virtualdrive
+          vd = @lsi.add_virtualdrive(component)
+        elsif component.type_of? :adapter
           @lsi.add_adapter(component)
         end
-        puts "         + context: #@context"
+        puts "           + context: #{@context[:stack]}"
       end
 
       def current
-        @context[-1]
+        @context[:stack][-1]
+      end
+
+      def adapter
+        @context[:adapter]
+      end
+
+      def virtualdrive
+        @context[:virtualdrive]
+      end
+
+      def physicaldrive
+        @context[:physicaldrive]
       end
 
     end
@@ -75,7 +91,7 @@ module Elesai
     def on_adapter_entry(old_state, event, *args)
       puts "        [#{current_state}] on_entry: leaving #{old_state}; args: #{args}"
 
-      @context.close if @context.current.kind_of? LSIArray::Adapter
+      @context.close unless @context.current.nil? or @context.current.type_of? :adapter
       adapter = args[0]
       @context.open adapter
 
@@ -88,12 +104,19 @@ module Elesai
 
     # Virtual Drive
 
-    def virtualdrive_line(virtualdrive,attr,value)
+    def virtualdrive_line(virtualdrive,key,value)
       puts "  [#{current_state}] event: virtualdrive_line: new #{virtualdrive.inspect}"
+      virtualdrive[key.to_sym] = value.to_i
     end
 
     def on_virtualdrive_entry(old_state, event, *args)
       puts "        [#{current_state}] on_entry: leaving #{old_state}; args: #{args}"
+
+      unless @context.current.nil?
+        if @context.current.type_of? :virtualdrive
+          @context.close
+        end
+      end
       virtualdrive = args[0]
       @context.open virtualdrive
     end
@@ -130,22 +153,24 @@ module Elesai
       puts "        [#{current_state}] entry: leaving #{old_state}; args: #{args}"
 
 
-      c = current_state.meta[:context][-1]
+      c = @context.current
       k = args[0].to_sym
       v = args[1]
 
       # Some attributes require special treatment for our purposes
 
       case k
-        when :coercedsize, :noncoercedsize, :rawsize
+        when :coercedsize, :noncoercedsize, :rawsize, :size
           m = /(?<number>[0-9\.]+)\s+(?<unit>[A-Z]+)/.match(v)
           v = LSIArray::PhysicalDrive::Size.new(m[:number],m[:unit])
         when :raidlevel
           m = /Primary-(?<primary>\d+),\s+Secondary-(?<secondary>\d+)/.match(v)
-          v = LSIArray::PhysicalDrive::RaidLevel.new(m[:primary],m[:secondary])
+          v = LSIArray::VirtualDrive::RaidLevel.new(m[:primary],m[:secondary])
         when :firmwarestate
           state,spin = v.gsub(/\s/,'').split(/,/)
           v = LSIArray::PhysicalDrive::FirmwareState.new(state.gsub(/\s/,'_').downcase.to_sym,spin.gsub(/\s/,'_').downcase.to_sym)
+        when :state
+          v = v.gsub(/\s/,'_').downcase.to_sym
         when :mediatype
           v = v.scan(/[A-Z]/).join
         when :inquirydata
@@ -156,7 +181,7 @@ module Elesai
 
     def on_attribute_exit(new_state, event, *args)
       puts "      [#{current_state}] exit: entering #{new_state} throught event #{event}; args: #{args}"
-      @context.close  unless event == :attribute_line
+      @context.close if @context.current.type_of? :physicaldrive and event != :attribute_line
 
       @context.flash!(new_state)
     end
@@ -169,8 +194,9 @@ module Elesai
 
     def on_exit_entry(new_state, event, *args)
       puts "      [#{current_state}] exit: entering #{new_state} throught event #{event}; args: #{args}"
-      puts "         + context: #{current_state.meta[:context]}"
-      @context.close
+      until @context.current.nil? do
+        @context.close
+      end
     end
 
     ### Regular Expression Match Handlers
@@ -188,7 +214,7 @@ module Elesai
     def virtualdrive_match(match)
       key = match[:key].gsub(/\s+/,"").downcase
       value = match[:value]
-      virtual_driveline!(LSIArray::VirtualDrive.new,key,value)
+      virtualdrive_line!(LSIArray::VirtualDrive.new,key,value)
     end
 
     # Physical Drive
@@ -235,7 +261,7 @@ module Elesai
 
   end
 
-  class PDinfo_aAll < Megacli
+  class PDlist_aAll < Megacli
 
     workflow do
 
@@ -276,10 +302,53 @@ module Elesai
 
   end
 
-#  l = LSIArray.new
-#  megacli = PDinfo_aAll.new
-#  megacli.parse! l, MEGACLI_PDINFO_AALL_OUT
+  class LDPDinfo_aAll < Megacli
 
-#  ap l.physicaldrives.size
+    workflow do
+
+      state :start do
+        event :adapter_line, :transitions_to => :adapter
+        event :exit_line, :transitions_to => :exit
+      end
+
+      state :adapter do
+        event :adapter_line, :transitions_to => :adapter
+        event :attribute_line, :transitions_to => :attribute
+        event :virtualdrive_line, :transitions_to => :virtualdrive
+        event :exit_line, :transitions_to => :exit
+      end
+
+      state :physicaldrive do
+        event :attribute_line, :transitions_to => :physicaldrive
+        event :exit_line, :transitions_to => :exit
+        event :adapter_line, :transitions_to => :adapter
+        event :physicaldrive_line, :transitions_to => :physicaldrive
+        event :attribute_line, :transitions_to => :attribute
+      end
+
+      state :virtualdrive do
+        event :physicaldrive_line, :transitions_to => :physicaldrive
+        event :attribute_line, :transitions_to => :attribute
+      end
+
+      state :attribute do
+        event :attribute_line, :transitions_to => :attribute
+        event :virtualdrive_line, :transitions_to => :virtualdrive
+        event :physicaldrive_line, :transitions_to => :physicaldrive
+        event :adapter_line, :transitions_to => :adapter
+        event :exit_line, :transitions_to => :exit
+      end
+
+      state :exit
+
+      on_transition do |from, to, triggering_event, *event_args|
+        #puts self.spec.states[to].class
+        # puts "    transition: #{from} >> #{triggering_event}! >> #{to}: #{event_args.join(' ')}"
+        #puts "                #{current_state.meta}"
+      end
+    end
+
+  end
+
 
 end
